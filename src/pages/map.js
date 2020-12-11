fbdd4e0582863f3857e67e5adfcace99e8b68da3
mikewiner <winer.mike@gmail.com>
2020-12-11 12:55:43 -0500

    bottom margin

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,16 +1,32 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { Link } from "gatsby";
+import website from "../../config/website";
 
 import Map from "../components/Map";
 
 const MapTitle = styled.h1`
+  @media all and (max-width: 800px) {
+    display: none;
+  }
+`;
+const MapHeader = styled.div`
   text-align: center;
-  /* color: orange; */
+  a {
+    color: orange;
+    &:visited,
+    &:hover {
+      color: orange;
+    }
+  }
 `;
 
-const MapPage = () => (
+const MapPage = (props) => (
   <div>
-    <MapTitle>Wow nice map, much markers</MapTitle>
+    <MapHeader>
+      <MapTitle>Wow nice map, much markers</MapTitle>
+      <Link to={`/`}>Go home</Link>
+    </MapHeader>
     <Map />
   </div>
 );
